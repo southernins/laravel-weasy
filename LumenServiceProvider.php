da26421d7d2c2b51fe6e7187f38d4e94d2d38146
Nathan Crews <ncrews@southerninsurance.net>
2023-06-28 14:09:13 -0500

    port to weasyprint

---
+++ LumenServiceProvider.php
@@ -0,0 +1,62 @@
+<?php namespace SouthernIns\LaravelWeasy;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class LumenServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->configure('weasy');
+
+        $configPath = __DIR__ . '/../config/weasy.php';
+        $this->mergeConfigFrom($configPath, 'weasy');
+    }
+
+    public function boot()
+    {
+        if ($this->app['config']->get('weasy.pdf.enabled')) {
+            $this->app->bind('weasy.pdf',function ($app) {
+                $binary = $app['config']->get('weasy.pdf.binary', '/usr/local/bin/weasyprint');
+                $options = $app['config']->get('weasy.pdf.options', array());
+                $env = $app['config']->get('weasy.pdf.env', array());
+                $timeout = $app['config']->get('weasy.pdf.timeout', false);
+
+                $weasy = new IlluminateSnappyPdf($app['files'], $binary, $options, $env);
+                if (false !== $timeout) {
+                    $weasy->setTimeout($timeout);
+                }
+
+                return $weasy;
+            });
+
+            $this->app->bind('weasy.pdf.wrapper',function ($app) {
+                return new PdfWrapper($app['weasy.pdf']);
+            });
+            $this->app->alias('weasy.pdf.wrapper', 'SouthernIns\LaravelWeasy\PdfWrapper');
+        }
+
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('weasy.pdf', 'weasy.pdf.wrapper' );
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# laravel-weasy
+# PHP Weasyprint wrapper for Laravel
  
+This package is a clone/test to see if a project using wkhtmltopdf based laravel-snappy can be ported to weasyprint.
+
+I have been using barryvdh/laravel-snappy to generate PDF's in one of my projects for years now.  this setup has done a fabulous job, but I'm starting to have issues newer css like bootstrap 5.   Based on https://wkhtmltopdf.org/status.html it has been time to look for a new solution. After reading and researching I've worked lots of options but have been coming up short.
+
+just recently I've come across the pontedilana/php-weasyprint project which is built as a drop in replacement for KnpLabs/snappy. Turns out installing weasyprint is just as easy as installing wkhtmltopdf was in my environment.
+
+    apt-get install -y weasyprint
+    ln -s /usr/bin/weasyprint  /usr/local/bin/weasyprint
+
+
+looking at the barryvdh/laravel-snappy code base it appears to be a pretty easy port, but enough of a change that laravel-snappy won't be able to support both snappy and php-weasyprint by configuration.    so here we are just testing some things out.
+
+The work here is basically a clone of barryvdh/laravel-snappy with changes to port over to weasyprint.  Thanks to the good work by the laravel-snappy, php-weasyprint and weasyprint projects most of what I have done here has been find and replace.
